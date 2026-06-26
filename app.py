5bdf6f98d57fc5c5cabba6cb8bf2e339266a8ab2
brewyne <muchoridenni5s@gmail.com>
2026-06-26 12:04:17 +0300

    1finalcommit

--- app.py
+++ app.py
@@ -10,19 +10,257 @@ app=Flask(__name__)
 def signup():
     # get user inputs from the form 
     username=request.form["username"]
-    email=request.form["email"]
     password=request.form["password"]
+    email=request.form["email"]
     phone=request.form["phone"]
 
     # connect to database
-    connect=pymysql.connect(
-    host="root",
-    user="localhost",
-    password="",
-    database="modcomdennis"
-)
+    connection=pymysql.connect(
+       host="localhost",
+       user="root",
+       password="",
+       database="modcomdennis")
+
+    #define the cursor
+    cursor= connection.cursor()
+
+    #define sql to insert user
+    sql="INSERT INTO users(username,password,email,phone)values(%s,%s,%s,%s)"  
+    #define  data coming from the form
+    data=(username, password, email, phone)
+
+    #by use of the cursor, execute the sql
+    cursor.execute(sql,data)
+    #commit the changes  to database
+    connection.commit()
+
+    return jsonify({"message":"user registered successfully"})
+# member signin/login
+# define route/endpoint
+@app.route("/api/signin",methods=["post"])
+#define the function
+def signin():
+   #get the user input from the form 
+   email=request.form["email"]
+   password=request.form["password"]
+   #connection to database
+   connection=pymysql.connect(
+       host="localhost",
+       user="root",
+       password="",
+       database="modcomdennis")
+    #define the cursor
+   cursor=connection.cursor(pymysql.cursors.DictCursor)
+   #define sql to select users
+   sql="select *from users where email=%s and password=%s"
+   #define your data
+   #nb its coming from step3
+   data=(email,password)
+   #execute/run query
+   cursor.execute(sql,data)
+   #wrong emailand password
+   if cursor.rowcount==0:
+       return jsonify({"message":"invalid imail or password"})
+   #correct imail and password
+   if cursor.rowcount==1:
+       #fech the user
+       user=cursor.fetchone()
+       return jsonify({"message":"log in successful","user":user})
+
+
+
+
+
+
+
+
+
+      
+
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
